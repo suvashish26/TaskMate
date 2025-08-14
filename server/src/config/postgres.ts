839be68e4a204b3d1da9a191f13dedeb695ee509
suvashish26 <shresthasuvashish@gmail.com>
2025-08-14 13:30:12 +0545

    chores: chores env variables

--- server/src/config/postgres.ts
+++ server/src/config/postgres.ts
@@ -1,16 +1,22 @@
 import { Sequelize } from "sequelize";
 import { createUserModel } from "../models/userSchema";
-const sequelize = new Sequelize("postgres", "postgres", "shtha@28", {
-  host: "localhost",
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD as string,
+  {
+    host: "localhost",
+    dialect: "postgres",
+    logging: false,
+  }
+);
 // Define UserModel immediately
 const UserModel = createUserModel(sequelize);
 
 export const connection = async (): Promise<void> => {
   try {
     await sequelize.authenticate();
-    console.log("✅ Connection has been established successfully.");
+    console.log("Connection has been established successfully.");
     await sequelize.sync();
     console.log("Database Synced");
   } catch (error) {
